45567855127a9881a77ce6e7a0bee5fb1d836605
StudentDane <dane.gillis@student.howest.be>
2019-06-02 12:33:07 +0200

    Add Service BeheerSpeler incl basic functionality

--- 06c-HighScores-Start/06c-HighScores-Start/Highscore.Lib/Services/BeheerSpelers.cs
+++ 06c-HighScores-Start/06c-HighScores-Start/Highscore.Lib/Services/BeheerSpelers.cs
@@ -10,10 +10,50 @@ namespace Highscore.Lib.Services
     public class BeheerSpelers
     {
         public List<Speler> Spelers { get; set; }
+        private int highestScoreEver { get; set; }
 
         public BeheerSpelers()
         {
             Spelers = new List<Speler>();
+            MaakSpelers();
+        }
+
+        public void MaakSpelers()
+        {
+            Speler speler;
+
+            speler = new Speler("Dane");
+            Spelers.Add(speler);
+
+            speler = new Speler("Kyle");
+            Spelers.Add(speler);
+
+            speler = new Speler("Tom");
+            Spelers.Add(speler);
+
+            speler = new Speler("Slava");
+            Spelers.Add(speler);
+        }
+
+        public void VoegSpelerToe(string naam, int highscore) {
+            Speler speler = new Speler(naam, highscore);
+            Spelers.Add(speler);
+        }
+
+        public int GetHighestScore()
+        {
+            int highestScore = 0;
+            foreach (Speler speler in Spelers)
+            {
+                if (speler.Highscore > highestScore) highestScore = speler.Highscore;
+            }
+            if (highestScore > highestScoreEver) highestScoreEver = highestScore;
+            return highestScore;
+        }
+
+        public int HighestScoreEver()
+        {
+            return highestScoreEver;
         }
     }
 }
