4bd4df5b3df64137a5bc6787f7ebc525ba01a728
StudentDane <dane.gillis@student.howest.be>
2019-06-02 12:13:03 +0200

    Add Speler.cs Entitity

---
+++ 06c-HighScores-Start/06c-HighScores-Start/Highscore.Lib/Entities/Speler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highscore.Lib.Entities
+{
+    public class Speler
+    {
+
+        public string Naam { get; set; }
+        public int Highscore { get; set; }
+
+        public Speler(string naam, int highscore)
+        {
+            Naam = naam;
+            Highscore = highscore;
+        }
+
+
+        public override string ToString()
+        {
+            string info;
+            info = $"{Naam} - {Highscore}";
+            return info;
+        }
+
+    }
+}
---
+++ 06c-HighScores-Start/06c-HighScores-Start/Highscore.Lib/Services/BeheerSpelers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Highscore.Lib.Entities;
+
+namespace Highscore.Lib.Services
+{
+    public class BeheerSpelers
+    {
+        public List<Speler> Spelers { get; set; }
+
+        public BeheerSpelers()
+        {
+            Spelers = new List<Speler>();
+        }
+    }
+}
